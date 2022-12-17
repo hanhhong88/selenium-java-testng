019d232dfdce86490afff2ff4c0ec485082d3f1c
hanhhong88 <hanh.nguyenthihong88@gmail.com>
2022-12-17 16:44:04 +0700

    Update more TC fahasha for Topic 13

--- src/webdriver/Topic_13_Action.java
+++ src/webdriver/Topic_13_Action.java
@@ -4,12 +4,9 @@ package webdriver;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -48,7 +45,7 @@ public class Topic_13_Action {
 
 	}
 	
-	@Test
+	//@Test
 	public void TC_02_MynTra_Shopping() {
 		driver.get("https://www.myntra.com/");
 		action.moveToElement(driver.findElement(By.xpath("//div[@class = 'desktop-navLink']//a[text() = 'Kids']"))).perform();
@@ -58,8 +55,22 @@ public class Topic_13_Action {
 		Assert.assertEquals(driver.findElements(By.cssSelector("span.breadcrumbs-crumb")), "Kids Home Bath");
 		
 	}
-
 	
+	@Test
+	public void TC_03_Fahasha_Shopping() {
+		driver.get("https://www.fahasa.com/");
+		//wait to manually close pop up bc currently haven't learn to handle it
+		Sleepinsecond(10);
+		//1st hover
+		action.moveToElement(driver.findElement(By.cssSelector("span.icon_menu"))).perform();
+		Sleepinsecond(3);
+		// 2nd hover
+		action.moveToElement(driver.findElement(By.xpath("//span[text() = 'Sách Trong Nước']"))).perform();
+		//click 1 option
+		driver.findElement(By.xpath("//div[contains(@class,'fhs_menu_content')]//a[text() = 'Quản Trị - Lãnh Đạo']")).click();
+		//Verify 
+		Assert.assertTrue((driver.findElement(By.xpath("//ol[@class ='breadcrumb']//strong[text() = 'Quản Trị - Lãnh Đạo']"))).isDisplayed());
+	}
 	
 	public void Sleepinsecond(long Timeinseconds) {
 		try {
@@ -69,6 +80,7 @@ public class Topic_13_Action {
 			e.printStackTrace();
 		}
 	}// 1000ms = 1s
+	
 	@AfterClass
 	public void afterClass() {
 		//driver.quit();
